35a014b48ab1eca8c8886ae94088891a1da94592
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-12-30 00:54:26 -0800

    Big ole mess

---
+++ distribution.py
@@ -0,0 +1,45 @@
+import csv
+from io import BytesIO
+from shutil import copyfile
+from urllib import request
+from zoneinfo import ZoneInfo
+
+from openpyxl import load_workbook
+
+PATH_TO_EXCEL = (
+    "https://www.dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-County.xls"
+)
+CENTRAL_TZ = ZoneInfo("America/Chicago")
+LATEST_PATH = "distribution/latest.csv"
+
+
+def main():
+    # load the file and convert it into a file-object
+    response = request.urlopen(PATH_TO_EXCEL)
+    file = BytesIO(response.read())
+
+    # pass into openpyxl and find when it was last modified
+    wb = load_workbook(file, read_only=True, data_only=True)
+    # we convert it to Central time because Texas (sorry, El Paso)
+    modified = wb.properties.modified.astimezone(tz=CENTRAL_TZ)
+    # now we have a timezone aware ISO date
+    as_of = modified.date().isoformat()
+
+    # grab our "Data" sheet
+    sheet = wb["Data"]
+
+    # save out our latest fil
+    with open(LATEST_PATH, "w") as outfile:
+        writer = csv.writer(outfile)
+
+        for row in sheet.rows:
+            writer.writerow(cell.value for cell in row)
+
+    # don't need the notebook anymore
+    wb.close()
+
+    copyfile(LATEST_PATH, f"distribution/snapshots/{as_of}.csv")
+
+
+if __name__ == "__main__":
+    main()
--- providers.sh
+++ providers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # the raw output JSON
-FILE=latest.json
+FILE=providers/latest.json
 
 LAST_EDITED=$(curl "https://services5.arcgis.com/ACaLB9ifngzawspq/arcgis/rest/services/VaccineProviderLocations/FeatureServer/0/?f=json" | jq ".editingInfo.lastEditDate")
 RAW_JSON=$(curl "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/VaccineProviderLocations/FeatureServer/0/query?where=1%3D1&outFields=*&maxRecordCountFactor=5&f=json" | jq "[.features | .[] | .attributes] | sort_by(.OBJECTID)")
 
-echo -n "$RAW_JSON" > "snapshots/$LAST_EDITED.json"
+echo -n "$RAW_JSON" > "providers/snapshots/$LAST_EDITED.json"
 echo -n "$RAW_JSON" > "$FILE"
